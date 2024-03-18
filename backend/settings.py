604cc2949a994ea20975ca8acea5124287c4823c
shizhanwang <shizhanwang30@gmail.com>
2024-03-18 13:57:20 +0800

    修改 DEBUG = True

--- backend/settings.py
+++ backend/settings.py
@@ -28,11 +28,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = "django-insecure-gd7$&)l40!d2m-i86h08d!o4ci3d)i&n7=u1t1#rgs1fwqom8h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
-APPEND_SLASH = True  # This is the default value
 
 # Application definition
 
